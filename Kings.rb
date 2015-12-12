75b1345c63fc0b86c45c2a60cfdbdb6425ee94df
yung9300 <BlakeNgYuanYao@gmail.com>
2015-12-12 09:11:01 +0800

    Kings.rb

---
+++ Kings.rb
@@ -0,0 +1,83 @@
+puts "Welcome to the Daily Misadventures of Chen Wei. You will be taking on the roll of Chen Wei, one of the four kings of Kuala Lumpur."
+puts "press enter when ready"
+input = gets.chomp!
+sleep(1)
+puts "You are the King of Kajang(KK), throughout your journey you will face the remaining 3 kings, KS, KD and KA as you battle for dominance over KL."
+puts "press enter when ready"
+input = gets.chomp!
+sleep(1)
+puts "But first, let us establish your information and preferences."
+puts "press enter when ready"
+input = gets.chomp!
+sleep(1)
+puts "You decide it's time to buy a new Honda Civic. Would you like the (FB or FD)? be warned that you will only be able to join one of these Facebook groups."
+civic = gets.chomp!
+sleep(2)
+puts "You pick up your civic #{civic} from the dealership. As you are about to leave, a silver car with a broken headlight pulls up."
+puts "press enter when ready"
+input = gets.chomp!
+sleep(1)
+puts "Nick steps out of the car. Nick knows everything about everything, whether its cars, the gym or drugs, think of him as your own Wikipedia."
+puts "press enter when ready"
+input = gets.chomp!
+sleep(1)
+puts "Nick: I heard you were just given the title of 4th King. I came to warn you about the First King, King of Subang. He hates it when the circle of kings expands and does his best to destroy new kings. Hence his nickname, King Crusher and Midget of Destruction."
+puts "press enter when ready"
+input = gets.chomp!
+sleep(1)
+puts "A BMW suv pulls up and the shortest man you've ever seen comes out of the car. You know the man as Derek, but others know him as the first king. His presence is immediately felt by those around as they fear the destruction that follows him."
+puts "press enter when ready"
+input = gets.chomp!
+sleep(1)
+puts "Do you decide to (speak or run)."
+decision1= gets.chomp!
+if decision1 == "run"
+	puts "you ran away. Nick follows you."
+	sleep(1)
+elsif decision1 == "speak"
+    puts "What do you say?"
+    p reply = gets.chomp!
+    sleep(2)
+    puts "Derek: I dont care what you have to say. I just came to tell you that you wont be holding that title for long. The circle of kings have always been held by 3 kings, there has never been a 4th who could sustain our lifestyle."
+    puts "press enter when ready"
+    input = gets.chomp!
+    sleep(1)	
+    puts "He jumps back into his suv, yes jumps, because he cannot reach his seat without jumping. He speeds off as if he owns the roads(according to Nick, he actually owns the roads)."
+    puts "press enter when ready"
+    input = gets.chomp!
+    sleep(1)
+end
+puts "Nick: There is a party tonight at Play:The Roof, it is probably the best time for you to reveal your identity to the public as the new King."
+puts "press enter when ready"
+input = gets.chomp!
+sleep(1)
+puts "You believe it is a good idea and you call Play. But you have difficulty deciding on how many bottles of Dom Perignon you need to reserve for tonight. Would you like (20,100 or 500)"
+bottles = gets.chomp!
+sleep(1)
+puts "Do you wear a (suit)? which shows superiority and class or a (t-shirt) which shows you're a boss and can rock up to a club in shorts and slippers?"
+outfit = gets.chomp!
+sleep(1)
+puts "The time has come for your debut as a king. It is 11pm and the club is packed. You get the DJ to stop the music as you announce you are giving away #{bottles} bottles of Dom Perignon tonight."
+puts "press enter when ready"
+input = gets.chomp!
+sleep(1)
+puts "Wah so rich ah today? not bad wei. Dont worry, I'll pay for my share."
+puts "press enter when ready"
+input = gets.chomp!
+sleep(1)
+puts "A man hands you 10 ringgit and says: now we're even."
+puts "press enter when ready"
+input = gets.chomp!
+sleep(1)
+puts "You know the man as Daryl. Others know him as the King of Damansara, nicknamed the King who honestly doesnt give a fuck. He does whatever he wants to do and trolls those around him into frustration. He sometimes pretends to work in Pavillion for fun. Staying around him for more than 5 minutes causes an innate desire to fuck around."
+puts "press enter when ready"
+input = gets.chomp!
+sleep(1)
+puts "Daryl: Fuiyoh, your #{outfit} damn nice wei. I mou jin eh, cannot afford."
+puts "press enter when ready"
+input = gets.chomp!
+sleep(1)
+puts "Daryl: Eh, be right back, I go say hi to some friends.(He doesn't come back)."
+sleep(1)
+puts "End of part 1"
+sleep(10)
